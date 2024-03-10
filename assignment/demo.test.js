8e239a6b118014964986161f6b134a506f2f55c5
Esther Chin <esther.chin@tjc.org>
2024-03-11 00:53:03 +0800

    modified demo.test.js to include both unit testings

--- assignment/demo.test.js
+++ assignment/demo.test.js
@@ -2,7 +2,7 @@
 // in this case, the files q2-arrays-loops and q3-objects are modules for this file.
 // const {} is what we call object destructuring and it allows us to unpack properties from objects into variables.
 const {studentList, printStudentNames} = require("./q2-arrays-loop");
-//const {convert} = require("./q3-objects");
+const {convert} = require("./q3-objects");
 
 //The describe () creates a block that groups together several related tests
 describe("Demo", ()=>{
@@ -16,14 +16,14 @@ describe("Demo", ()=>{
         expect(spyFn).toHaveBeenCalledTimes(studentList.length);
     })
 
-    // it("should test the convert function of q3", ()=>{
-    //     const keyArr = ["NLB1", "NLB2"];
-    //     const valueArr = ["Book A", "Book B"];
+    it("should test the convert function of q3", ()=>{
+        const keyArr = ["NLB1", "NLB2"];
+        const valueArr = ["Book A", "Book B"];
 
-    //     const result = convert(keyArr, valueArr);
-    //     //the toBe() can be used to match specific values
-    //     expect(result["NLB1"]).toBe("Book A");
-    //     expect(result["NLB2"]).toBe("Book B");
-    // })
+        const result = convert(keyArr, valueArr);
+        //the toBe() can be used to match specific values
+        expect(result["NLB1"]).toBe("Book A");
+        expect(result["NLB2"]).toBe("Book B");
+    })
 })
 //So when the command `npm run test` is run, it calls the test suite and runs the individual tests.
